21941abb6eb808f2e01d700b913b1bcf03e965ff
Sveinbjorn Geirsson <sveinbjorn@sveinbjorn.dev>
2025-01-29 16:30:35 +0000

    merge

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: './', // Add this line for GitHub Pages
+  base: './',  
   plugins: [react()],
   resolve: {
     alias: {
